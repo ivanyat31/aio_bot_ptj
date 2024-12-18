beedc60d0e83ccbe2b8457a35838e57c9aea7a10
ivanyat31 <kyivstarlenina41@gmail.com>
2024-12-18 22:13:10 +0300

    Initial commit

---
+++ keyboards/default/markups.py
@@ -0,0 +1,42 @@
+from aiogram.types import ReplyKeyboardMarkup
+
+back_message = '👈 Назад'
+all_right_message = '✅ Все верно'
+cancel_message = '🚫 Отменить'
+
+
+
+def back_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add(back_message)
+
+    return markup
+
+
+def check_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.row(back_message, all_right_message)
+
+    return markup
+
+confirm_message = '✅ Подтвердить заказ'
+def confirm_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add(confirm_message)
+    markup.add(back_message)
+
+    return markup
+
+
+def submit_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.row(cancel_message, all_right_message)
+
+    return markup
+
+
+def submit_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.row(cancel_message, all_right_message)
+
+    return markup
